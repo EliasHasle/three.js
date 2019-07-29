42d8cf7c56cd5de81d9690122d6a50f2eada51d4
Elias Hasle <elias.hasle@gmail.com>
2019-07-29 02:09:36 +0200

    Added needsUpdate checks to WebGLRenderer.compile
    Before: initMaterial is called on every instance of every material:
    1. initMaterial invariably calls programCache.getProgramCode, which builds a large string from the shader.
    2. Then the WebGLProperties object is queried with the material to obtain a copy of the previous code used for this material.
    3. If a code is found and it matches the one newly built, done.
    4. If, on the other hand, no code is found or they do not match, the code string is rebuilt once more after calling material.onBeforeCompile (NOTE: This could have been avoided if onBeforeCompile were null or undefined by default and the onBeforeCompile and getProgramCode calls were wrapped in a conditional),
    5. and then programCache.acquireProgram is called.
    6. acquireProgram invariably loops over the array of program codes, checking the new code against them, and breaking early only if it finds a match. The array is not ordered by usage frequency, so one can expect to loop over on average half the programs if there is a match, and (invariably) all the programs if there is no match. (Many string comparisons will obviously break early, or be "optimized away" by a good JS implementation.)
    
    After: Shared materials will be `initMaterial`ed only once.
    
    Performance: Not measured.
    
    Side effects: Not tested.

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1037,14 +1037,22 @@ function WebGLRenderer( parameters ) {
 
 					for ( var i = 0; i < object.material.length; i ++ ) {
 
-						initMaterial( object.material[ i ], scene.fog, object );
+						if ( object.material[ i ].needsUpdate ) {
+							
+							initMaterial( object.material[ i ], scene.fog, object );
+							
+						}
 
 					}
 
 				} else {
-
-					initMaterial( object.material, scene.fog, object );
-
+					
+					if ( object.material.needsUpdate ) {
+						
+						initMaterial( object.material, scene.fog, object );
+						
+					}
+					
 				}
 
 			}
